1060356d612e128f70d2ab50239b09348171f553
Kazuya Kaneko <kazuya.kaneko@fidx.co.jp>
2026-01-05 01:08:36 +0900

    how may divisors改善

--- AOJ/how_many_divisors/how_many_divisors.hs
+++ AOJ/how_many_divisors/how_many_divisors.hs
@@ -1,11 +1,4 @@
 main :: IO ()
 main = do
-  [a, b, c] <- map read . words <$> getLine :: IO [Int]
-
-  let ans = divisors c [a .. b]
-
-  print ans
-
-divisors :: Int -> [Int] -> Int
-divisors _ [] = 0
-divisors y (x : xs) = (if y `mod` x == 0 then 1 else 0) + divisors y xs
+  [a, b, c] <- map read . words <$> getLine
+  print $ length [x | x <- [a .. b], c `mod` x == 0]
