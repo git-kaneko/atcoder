9573a4e0d636049eda4b006139d3840302a8250a
Kazuya <kazuz4@yahoo.co.jp>
2026-01-25 23:29:05 +0900

    改善

--- AOJ/print_a_rectangle/print_a_rectangle.hs
+++ AOJ/print_a_rectangle/print_a_rectangle.hs
@@ -1,7 +1,4 @@
 main :: IO ()
-main = do
-  interact $ concatMap (\[h, w] -> output w h) . takeWhile (/= [0, 0]) . map (map read . words) . lines
-
-output :: Int -> Int -> String
-output _ 0 = "\n"
-output w h = replicate w '#' ++ "\n" ++ output w (h - 1)
+main = interact $ concatMap draw . takeWhile (/= [0, 0]) . map (map read . words) . lines
+  where
+    draw [h, w] = unlines (replicate h (replicate w '#')) ++ "\n"
