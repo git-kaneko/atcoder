2d3431012575aa185b71b17bf76abb97d9277be1
Kazuya Kaneko <kazuya.kaneko@fidx.co.jp>
2026-02-09 00:41:06 +0900

    改善

--- AOJ/print_a_frame/print_a_frame.hs
+++ AOJ/print_a_frame/print_a_frame.hs
@@ -1,4 +1,8 @@
 main :: IO ()
-main = interact $ concatMap draw . takeWhile (/= [0, 0]) . map (map read . words) . lines
+main = interact $ concatMap draw . takeWhile (/= (0, 0)) . map parseHW . lines
   where
-    draw [h, w] = replicate w '#' ++ "\n" ++ unlines (replicate (h - 2) ("#" ++ replicate (w - 2) '.' ++ "#")) ++ replicate w '#' ++ "\n\n"
+    parseHW s = let [h, w] = map read (words s) in (h, w)
+    draw (h, w) = unlines (top : replicate (h - 2) mid ++ [top, ""])
+      where
+        top = replicate w '#'
+        mid = '#' : replicate (w - 2) '.' ++ "#"
